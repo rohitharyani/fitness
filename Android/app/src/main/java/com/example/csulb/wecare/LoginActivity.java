a81f819e67dd1ebdd04c44c4648f38c43fad00dc
sankar <sankaran22.5@gmail.com>
2018-03-14 11:38:51 -0700

    Email login updated

--- Android/app/src/main/java/com/example/csulb/wecare/LoginActivity.java
+++ Android/app/src/main/java/com/example/csulb/wecare/LoginActivity.java
@@ -9,6 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -41,6 +46,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button mSignUpButton;
     private TextView mForgotLoginTextView;
 
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+
     private LoginButton loginFacebookButton;
     private FirebaseAuth mFacebookAuth;
     private CallbackManager mFacebookCallbackManager;
@@ -50,6 +59,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private static final String TAG1 = "Google Sign in";
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mGoogleAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +76,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mLoginBackground = (BlurImageView)findViewById(R.id.loginBackgroundImageView);
         mLoginBackground.setBlur(1);
 
+        auth = FirebaseAuth.getInstance();
+        inputEmail = (EditText) findViewById(R.id.loginEmailEditText);
+        inputPassword = (EditText) findViewById(R.id.loginPasswordEditText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         mLoginButton = (Button)findViewById(R.id.loginButton);
+
         mLoginButton.setOnClickListener(this);
         mSignUpButton = (Button)findViewById(R.id.loginSignUpButton);
         mSignUpButton.setOnClickListener(this);
@@ -100,7 +115,49 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     }
                 });
 
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
 
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
         //Facebook sign extends
 
         //Configure Google Sign in
